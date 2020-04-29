0ff20dec9fc9669612f29c0518d1520453a021cc
abykova <alex.94.girl@gmail.com>
2020-04-29 18:42:03 +0600

    update and show rau

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Show Rau</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Show Rau</a>
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <!-- END nav -->
+    
+    <!-- <div class="js-fullheight"> -->
+
+  <div class="hero-wrap js-fullheight">
+      <div class="overlay"></div>
+      <div id="particles-js"></div>
+      <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                      <a href="https://yadi.sk/i/3pdXjM_-32_PdA" target="_blank" class="block-20" style="background-image: url('images/image_1.jpg');">
+                      </a>
+                      <div class="text p-4 d-block">
+                          <div class="meta mb-3">
+                              <div><a href="https://yadi.sk/i/3pdXjM_-32_PdA" target="_blank">Aprel 30, 2020</a></div>
+                          </div>
+                          <h3 class="heading"><a href="https://yadi.sk/i/3pdXjM_-32_PdA" target="_blank" >
+                                  Скорей смотри новый номер, сладенький!
+                                  В студии апрельский выпуск. Продуктивного всем дня. Обняли виртуально.
+                                  #шаурау 🌚🌝
+                              </a></h3>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+    
+    <div class="bg-light">
+    <section class="ftco-section-featured ftco-animate">
+      <div class="container-fluid" data-scrollax-parent="true">
+        <div class="row no-gutters d-flex align-items-center" data-scrollax=" properties: { translateY: '-30%'}">
+
+          <div class="col-md-3 mb-3">
+            <a href="#" class="featured-img">
+              <div class="text-1 p-4 d-flex align-items-center">
+                <h3>Show Rau<br><span class="tag">April</span></h3>
+              </div>
+              <img src="images/image_1.jpg" class="img-fluid" alt="">
+              <div class="text p-4 d-flex align-items-center">
+                <div class="user d-flex align-items-center">
+                  <h3>Alexandra Asadchaya <br><span class="position">Designer</span></h3>
+                  <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                </div>
+              </div>
+            </a>
+          </div>
+          
+          <div class="col-md-6">
+            <div class="row no-gutters">
+              <div class="col-md-12">
+                <div class="row no-gutters d-flex align-items-end">
+
+                  <div class="col-md-8">
+                    <a href="https://yadi.sk/i/f6TuJDeN2G9slA" target="_blank" class="featured-img">
+                      <div class="text-1 p-4 d-flex align-items-center">
+                        <h3>Show Rau<br><span class="tag">December 2019</span></h3>
+                      </div>
+                      <img src="images/image_5.jpg" class="img-fluid" alt="">
+                      <div class="text p-4 d-flex align-items-center">
+                        <div class="user d-flex align-items-center">
+                          <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class="col-md-4">
+                    <a href="https://yadi.sk/i/YOoBWFTInk1Dug" target="_blank" class="featured-img">
+                      <div class="text-1 p-4 d-flex align-items-center">
+                        <h3>Show Rau<br><span class="tag">January 2020</span></h3>
+                      </div>
+                      <img src="images/image_4.jpg" class="img-fluid" alt="">
+                      <div class="text p-4 d-flex align-items-center">
+                        <div class="user d-flex align-items-center">
+
+                          <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <div class="row no-gutters d-flex align-items-start">
+                  <div class="col-md-8">
+                    <a href="https://yadi.sk/i/iCIqgSN3jkUc7Q" target="_blank" class="featured-img">
+                      <div class="text-1 p-4 d-flex align-items-center">
+                        <h3>Show Rau<br><span class="tag">November 2019</span></h3>
+                      </div>
+                      <img src="images/image_6.jpg" class="img-fluid" alt="">
+                      <div class="text p-4 d-flex align-items-center">
+                        <div class="user d-flex align-items-center">
+
+                          <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-md-4">
+                    <a href="https://yadi.sk/i/aIYC6NeNrGlM5A" target="_blank" class="featured-img">
+                      <div class="text-1 p-4 d-flex align-items-center">
+                        <h3>Show Rau<br><span class="tag">February 2020</span></h3>
+                      </div>
+                      <img src="images/image_3.jpg" class="img-fluid" alt="">
+                      <div class="text p-4 d-flex align-items-center">
+                        <div class="user d-flex align-items-center">
+
+                          <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <a href="https://yadi.sk/i/H71K4JwxgSiwGA" target="_blank" class="featured-img">
+              <div class="text-1 p-4 d-flex align-items-center">
+                <h3>Show Rau<br><span class="tag">March 2020</span></h3>
+              </div>
+              <img src="images/image_2.jpg" class="img-fluid" alt="">
+              <div class="text p-4 d-flex align-items-center">
+                <div class="user d-flex align-items-center">
+
+                  <h3>Alexandra Bykova <br><span class="position">Author</span></h3>
+                </div>
+              </div>
+            </a>
+          </div>
+
+        </div>
+        <div class="row mt-5 d-flex justify-content-center">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Bykova.</h2>
+<!--              <p>Здесь текст обо мне</p>-->
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+<!--              <h2 class="ftco-heading-2">Contact Information</h2>-->
+              <ul class="list-unstyled">
+<!--                <li><a href="#" class="py-2 d-block">Казахстан, Краганда, 100000</a></li>-->
+                <li><a href="#" class="py-2 d-block">Kazakstan, Qaraganda, 100000</a></li>
+<!--                <li><a href="#" class="py-2 d-block">+ 7 705 108 69 36</a></li>-->
+<!--                <li><a href="#" class="py-2 d-block">ya.abykova@yandex.kz</a></li>-->
+              </ul>
+            </div>
+          </div>
+<!--          <div class="col-md">-->
+<!--            <div class="ftco-footer-widget mb-4">-->
+<!--              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">-->
+<!--                <li class="ftco-animate"><a href="https://twitter.com/drinkuper" target="_blank" ><span class="icon-twitter"></span></a></li>-->
+<!--                <li class="ftco-animate"><a href="https://www.instagram.com/drinkuper/" target="_blank" ><span class="icon-instagram"></span></a></li>-->
+<!--              </ul>-->
+<!--            </div>-->
+<!--          </div>-->
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/particles.min.js"></script>
+  <script src="js/particle.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
